61d6bdd50a665bbc784742d53ee0e6ba300f1ae3
tayebie <emad@DESKTOP-OJB8KIL.localdomain>
2019-10-31 19:18:02 +0330

    added contact changes and few other changes.

--- assets/js/index.js
+++ assets/js/index.js
@@ -50,3 +50,51 @@ $(document).ready(function(){
 
   });
 });
+// formspree js start
+  window.addEventListener("DOMContentLoaded", function() {
+
+    // get the form elements defined in your form HTML above
+    
+    var form = document.getElementById("join-form");
+    var button = document.getElementById("join-form-button");
+    var status = document.getElementById("join-form-status");
+
+    // Success and Error functions for after the form is submitted
+    
+    function success() {
+      form.reset();
+      button.style = "display: none ";
+      status.innerHTML = "با تشکر از همراهی شما. به زودی با شما تماس گرفته خواهد شد.";
+    }
+
+    function error() {
+      status.innerHTML = "در فرایند ثبت فرم مشکلی به وجود آمده است. لطفا مجدداً تلاش کنید.";
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
+// formspree js end
+
